2217e15ec016dec03a26b3690c5f40764bd8e45e
Hai Pham <swiften.svc@gmail.com>
2020-08-29 11:48:32 +0800

    feat(graphql): export graphql schema

--- graphql.ts
+++ graphql.ts
@@ -1,5 +1,6 @@
 import GraphQLJSON from "graphql-type-json";
 export { ApolloServer, gql, makeExecutableSchema } from "apollo-server-express";
+export { GraphQLSchema } from "graphql";
 export { GraphQLDate, GraphQLDateTime } from "graphql-iso-date";
 export { GraphQLJSONObject } from "graphql-type-json";
 export { GraphQLJSON };
