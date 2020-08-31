db93a7450236b63ed88b0cc51bc272a98b733a5a
Hai Pham <swiften.svc@gmail.com>
2020-08-31 10:22:36 +0800

    feat(scripts): add scripts to authorize Google and sync sheet

---
+++ script/authorize_google.js
@@ -0,0 +1,76 @@
+const fs = require("fs-extra");
+const { google } = require("googleapis");
+const path = require("path");
+const readline = require("readline");
+
+/**
+ * @typedef Args
+ * @property {string[]} scopes
+ * @param {Args} args
+ */
+module.exports = async function ({ scopes }) {
+  const TOKEN_PATH = path.join("credential", "google_token.json");
+
+  // Load client secrets from a local file.
+  const credentialsContent = await fs.readFile(
+    path.join("credential", "google_credentials.json")
+  );
+
+  const credentials = JSON.parse(credentialsContent.toString("utf-8"));
+  const { client_secret, client_id, redirect_uris } = credentials.web;
+
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  /** @type {any} */
+  let tokens;
+
+  try {
+    const tokenString = await fs.readFile(TOKEN_PATH);
+    tokens = JSON.parse(tokenString.toString("utf-8"));
+
+    switch (true) {
+      case !!tokens.refresh_token:
+        break;
+
+      case tokens.expiry_date < new Date().getTime():
+        throw new Error("Tokens expired");
+
+      default:
+        break;
+    }
+  } catch (e) {
+    const authURL = oAuth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: scopes,
+    });
+
+    console.log("Authorize this app by visiting this url:", authURL);
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    tokens = await new Promise((resolve, reject) => {
+      rl.question("Enter the code from that page here: ", async (code) => {
+        rl.close();
+
+        try {
+          const { tokens } = await oAuth2Client.getToken(code);
+          // Store the token to disk for later program executions
+          await fs.writeFile(TOKEN_PATH, JSON.stringify(tokens));
+          resolve(tokens);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  }
+
+  oAuth2Client.setCredentials(tokens);
+  return oAuth2Client;
+};
---
+++ script/sync_google_sheet.js
@@ -0,0 +1,41 @@
+const { google } = require("googleapis");
+const authorizeGoogle = require("./authorize_google");
+
+/**
+ * @typedef SheetCallbackArgs
+ * @property {unknown[][]} sheetRows
+ * @property {string} sheetTitle
+ * @typedef Args
+ * @property {(args: SheetCallbackArgs) => Promise<void>} callback
+ * @property {string[]} [scopes]
+ * @property {string} spreadsheetID
+ * @param {Args} args
+ */
+module.exports = async function ({
+  callback,
+  scopes = [],
+  spreadsheetID: spreadsheetId,
+}) {
+  const oAuth2Client = await authorizeGoogle({ scopes });
+  const workbook = google.sheets({ auth: oAuth2Client, version: "v4" });
+
+  const {
+    data: { sheets: worksheets = [] },
+  } = await workbook.spreadsheets.get();
+
+  await Promise.all(
+    worksheets.map(async ({ properties: { title: sheetTitle = "" } = {} }) => {
+      if (!sheetTitle) return;
+
+      const {
+        data: { values: sheetRows },
+      } = await workbook.spreadsheets.values.get({
+        spreadsheetId,
+        range: `${sheetTitle}!A:Z`,
+      });
+
+      if (sheetRows == null || sheetRows.length === 0) return;
+      await callback({ sheetRows, sheetTitle });
+    })
+  );
+};
