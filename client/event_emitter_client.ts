5bd79baa89965a5280e8c15c66fd59f610afb09a
Hai Pham <swiften.svc@gmail.com>
2020-11-20 00:33:28 +0800

    test(event-emitter): add event emitter tests

--- client/event_emitter_client.ts
+++ client/event_emitter_client.ts
@@ -1,33 +1,28 @@
-import { EventEmitterClient } from "../interface";
+import { AnyClient, EventEmitterClient } from "../interface";
 
 /**
  * Provide a common interface for event emittance, with on/off for registering
  * event callbacks.
  */
-export default function <
-  Callbacks extends { [x: string]: (...args: any[]) => void }
->() {
-  const callbacks: Partial<{ [x in keyof Callbacks]: Callbacks[x][] }> = {};
+export default function <CB extends AnyClient>() {
+  const callbacks: Partial<{ [x in keyof CB]: CB[x][] }> = {};
 
-  const client: EventEmitterClient<Callbacks> = {
-    callbacksType: {} as Callbacks,
-    emit: function <K extends keyof Callbacks>(
-      key: K,
-      ...args: Parameters<Callbacks[K]>
-    ) {
+  const client: EventEmitterClient<CB> = {
+    callbacksType: {} as CB,
+    emit: function <K extends keyof CB>(key: K, ...args: Parameters<CB[K]>) {
       if (callbacks[key] == null) return;
       for (const callback of callbacks[key]!) callback(...args);
     },
-    getCallbackCount: function <K extends keyof Callbacks>(key: K) {
+    getCallbackCount: function <K extends keyof CB>(key: K) {
       return callbacks[key]?.length || 0;
     },
-    on: function <K extends keyof Callbacks>(key: K, callback: Callbacks[K]) {
+    on: function <K extends keyof CB>(key: K, callback: CB[K]) {
       if (callbacks[key] == null) callbacks[key] = [];
       callbacks[key]!.push(callback);
     },
-    off: function <K extends keyof Callbacks>(key: K, callback: Callbacks[K]) {
+    off: function <K extends keyof CB>(key: K, callback: CB[K]) {
       if (callbacks[key] == null) return;
-      const index = callbacks[key]!.findIndex((cb: any) => cb === callback);
+      const index = callbacks[key]!.findIndex((cb) => cb === callback);
       if (index === -1) return;
       callbacks[key]!.splice(index, 1);
     },
