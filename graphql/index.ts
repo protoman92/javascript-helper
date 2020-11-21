ebed2374cc01aea7128fda3fd2e305c14c94100a
Hai Pham <swiften.svc@gmail.com>
2020-11-22 03:26:10 +0800

    chore(dependency): select grapqhl-tools dependencies one by one

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,12 +1,9 @@
 import gql from "graphql-tag";
 import GraphQLJSON from "graphql-type-json";
-export {
-  ApolloServer,
-  makeExecutableSchema,
-  toApolloError,
-} from "apollo-server-express";
+export { makeExecutableSchema } from "@graphql-tools/schema";
+export { getDirectives, MapperKind, mapSchema } from "@graphql-tools/utils";
+export { ApolloServer, toApolloError } from "apollo-server-express";
 export { defaultFieldResolver, GraphQLSchema } from "graphql";
 export { GraphQLDate, GraphQLDateTime } from "graphql-iso-date";
-export { getDirectives, MapperKind, mapSchema } from "graphql-tools";
 export { GraphQLJSONObject } from "graphql-type-json";
 export { gql, GraphQLJSON };
