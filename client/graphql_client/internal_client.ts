e3e1d42ee3399def47bd8945864ac5ae1c4d1061
Hai Pham <swiften.svc@gmail.com>
2021-01-17 19:29:43 +0800

    feat(graphql): allow internal graphQL client to specify default context object

--- client/graphql_client/internal_client.ts
+++ client/graphql_client/internal_client.ts
@@ -1,22 +1,27 @@
 import { DocumentNode, graphql, GraphQLArgs, GraphQLSchema } from "graphql";
 
 export default function ({
+  defaultContextValues = {},
   schemaFn,
-}: Readonly<{ schemaFn: () => GraphQLSchema }>) {
+}: Readonly<{ defaultContextValues?: object; schemaFn: () => GraphQLSchema }>) {
   return {
     request: async function <A, R>({
+      contextValue = {},
       document,
       ...args
     }: Omit<GraphQLArgs, "schema" | "source" | "variableValues"> &
       Readonly<{ document: DocumentNode; variables: A }>) {
+      const requestContext = { ...defaultContextValues, ...contextValue };
+
       const { data, errors } = await graphql<R>({
         ...args,
+        contextValue: requestContext,
         schema: schemaFn(),
         source: document.loc!.source,
         variableValues: args.variables,
       });
 
-      if (!!errors && !!errors.length) throw errors[0];
+      if (errors != null && errors.length > 0) throw errors[0];
       return data!;
     },
   };
