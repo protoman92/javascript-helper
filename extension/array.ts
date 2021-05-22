be950a46552cc08170a4b14358b513012cbceee5
Hai Pham <swiften.svc@gmail.com>
2021-05-22 23:21:14 +0800

    feat(extension): add mergeElements method to array

--- extension/array.ts
+++ extension/array.ts
@@ -1,5 +1,9 @@
 import {} from "util";
 
+interface HasMergeableElements<T> {
+  mergeElements: () => T extends Record<string, any> ? T : never;
+}
+
 interface Indexable<T> {
   /** Get the element at a particular index. This method can return undefined */
   elementAtIndex(index: number): T | undefined;
@@ -11,8 +15,15 @@ interface SetConvertible<T> {
 }
 
 declare global {
-  interface ReadonlyArray<T> extends Indexable<T>, SetConvertible<T> {}
-  interface Array<T> extends Indexable<T>, SetConvertible<T> {}
+  interface ReadonlyArray<T>
+    extends HasMergeableElements<T>,
+      Indexable<T>,
+      SetConvertible<T> {}
+
+  interface Array<T>
+    extends HasMergeableElements<T>,
+      Indexable<T>,
+      SetConvertible<T> {}
 }
 
 Array.prototype.elementAtIndex = function (index) {
@@ -21,6 +32,16 @@ Array.prototype.elementAtIndex = function (index) {
 
 Array.prototype.first = function () {
   return this.elementAtIndex(0);
+};
+
+Array.prototype.mergeElements = function () {
+  let merged: Record<string, unknown> = {};
+
+  for (const element of this) {
+    merged = { ...merged, ...element };
+  }
+
+  return merged;
 };
 
 Array.prototype.toSet = function () {
