9ba33e1a14307a0796c7ef074993bc9c5c1a30e0
Hai Pham <swiften.svc@gmail.com>
2021-06-21 14:28:36 +0800

    feat(utils): add mockMap for test utils

--- test/utils.ts
+++ test/utils.ts
@@ -1,4 +1,5 @@
 import { DeepPartial } from "ts-essentials";
+import { MapKey, MapValue } from "../interface";
 
 export function mockSomething<T>(override: DeepPartial<T>): T {
   return override as T;
@@ -9,3 +10,13 @@ export function mockSomeParameters<FN extends (...args: any[]) => any>(
 ): Parameters<FN> {
   return override as Parameters<FN>;
 }
+
+interface MockMap {
+  <K, V>(...args: DeepPartial<[K, V][]>): Map<K, V>;
+
+  <M extends Map<any, any> | null | undefined>(
+    ...args: DeepPartial<[MapKey<M>, MapValue<M>]>[]
+  ): NonNullable<M>;
+}
+
+export const mockMap: MockMap = (...args: any[]) => new Map(args as any);
