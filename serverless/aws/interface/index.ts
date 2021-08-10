a5f4bacf90ffc23a6e32a6861f2f0949c6eb9b24
Hai Pham <swiften.svc@gmail.com>
2021-08-11 03:07:09 +0800

    feat: use express.Express instead of express.Application

--- serverless/aws/interface/index.ts
+++ serverless/aws/interface/index.ts
@@ -2,5 +2,5 @@ import express from "express";
 
 export interface ServerOrServerlessArgs {
   readonly defaultPort: number;
-  initServer(): Promise<express.Application>;
+  initServer(): Promise<express.Express>;
 }
