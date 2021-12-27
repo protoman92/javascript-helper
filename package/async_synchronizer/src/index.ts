4bfbcc5c6cd10b4ea7b938d42e2f0bad66543234
Hai Pham <swiften.svc@gmail.com>
2021-12-28 00:15:29 +0800

    feat(async-synchronizer): remove dependencies on essential-types and utils

--- package/async_synchronizer/src/index.ts
+++ package/async_synchronizer/src/index.ts
@@ -1,8 +1,4 @@
-import {
-  GenericAsyncFunction,
-  Promised,
-} from "@haipham/javascript-helper-essential-types";
-import { wrapResolvable } from "@haipham/javascript-helper-utils";
+import { Awaited } from "ts-essentials";
 
 /** Ensure only one request can be running at a time */
 export function createAsyncSynchronizer() {
@@ -10,10 +6,12 @@ export function createAsyncSynchronizer() {
   let isRunning = false;
 
   return {
-    synchronize: <FN extends GenericAsyncFunction>(fn: FN): FN => {
+    synchronize: <FN extends (...args: any[]) => Promise<any> | any>(
+      fn: FN
+    ): FN => {
       return function synchronized(this: any, ...args1: Parameters<FN>) {
         if (isRunning) {
-          return new Promise<Promised<ReturnType<FN>>>((resolve) => {
+          return new Promise<Awaited<ReturnType<FN>>>((resolve) => {
             requestQueue.push(() => {
               resolve(synchronized.call(this, ...args1));
             });
@@ -21,7 +19,7 @@ export function createAsyncSynchronizer() {
         } else {
           isRunning = true;
 
-          return wrapResolvable(fn.call(this, ...args1)).finally(() => {
+          return Promise.resolve(fn.call(this, ...args1)).finally(() => {
             isRunning = false;
             requestQueue.splice(0, 1)[0]?.call(undefined);
           });
